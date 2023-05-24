10419bdf16f41abcd2d925e7de709955fd808aad
Albina Zaidullina <albina.zaidullina@csssr.com>
2023-05-24 11:59:14 +0300

    update login_page test

--- pages/login_page.py
+++ pages/login_page.py
@@ -38,14 +38,3 @@ class Login(Base):
         field = self.browser.find_element(*login_page_locators.password)
         value = field.get_attribute('value')
         assert value == '', 'password filed wasn\'t empty'
-
-
-    def authorize(self, user, user_password):
-            user_name = self.browser.find_element(*authorization_page_locators.usrername)
-            user_name.send_keys(user)
-
-        password = self.browser.find_element(*authorization_page_locators.password)
-        password.send_keys(user_password)
-
-        confirm = self.browser.find_element(*authorization_page_locators.confirm_button)
-        confirm.click()
